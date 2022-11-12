0cf2c127e1ea7c2073f32332278647207ec6ecda
Vazgen <vzgo03@mail.ru>
2022-11-12 19:36:21 +0300

    создание

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == Маша)
+{
+    Console.WriteLine("Ура эТО Же МАША");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username)
+}
